e1f7d60aba9a53ad659b7af12ea4ba735b821d72
Autumn Welles <awelles@coalmarch.com>
2015-11-19 17:34:08 -0500

    got sass & watch working

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,27 @@
-var gulp = require('gulp');
+'use strict';
 
-gulp.task('default', function() {
-    
+var gulp = require('gulp'),
+    sass = require('gulp-sass'),
+    livereload = require('gulp-livereload'),
+    sassGlob = require('gulp-sass-glob'),
+    del = require('del');
+
+gulp.task('clean', function() {
+  return del([
+    'css'
+  ]);
 });
+
+gulp.task('sass', function() {
+  gulp.src('./sass/**/*.scss')
+    .pipe(sassGlob())
+    .pipe(sass.sync())
+    .pipe(gulp.dest('./css'));
+});
+
+gulp.task('watch', function() {
+  livereload.listen();
+  gulp.watch('./sass/**/*.scss', ['sass']);
+});
+
+gulp.task('default', ['sass', 'watch']);
